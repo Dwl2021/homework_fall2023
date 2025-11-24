80eea61ad19091356bb17619926473b5200018cb
Weiliang Deng <Dengwl.2021@gmail.com>
2025-11-24 10:27:44 +0800

    finish hw2 s6

---
+++ hw2/run_s6.sh
@@ -0,0 +1,1 @@
+python cs285/scripts/run_hw2.py --env_name InvertedPendulum-v4 -n 100 --exp_name pendulum_default_s -rtg --use_baseline -na --batch_size 5000 -lr 0.01 --gae_lambda 0.99 --discount 0.9
